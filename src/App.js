d20c2d712ac0f3886f3e4731aecd08563e1fe0ba
TobiOladoja <tbfullstack@gmail.com>
2020-04-27 11:45:18 +0100

    Axios console log testing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 import './App.css';
 
 class App extends Component {
   state = {
     advice: '',
+  };
+
+  componentDidMount() {
+    this.fetchAdvice();
+  }
+
+  fetchAdvice = () => {
+    axios
+      .get('https://api.adviceslip.com/advice')
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   };
 
   render() {
